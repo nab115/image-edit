349a62eb16d1e1ca7d80a049d9007bcfb00183b5
Naran Babha <nab115@pitt.edu>
2022-11-25 08:33:41 -0800

    Get method for retrieving image

--- src/main/java/com/example/imageedit/controllers/MainController.java
+++ src/main/java/com/example/imageedit/controllers/MainController.java
@@ -25,24 +25,34 @@ public class MainController {
         return "upload";
     }
 
+    @GetMapping("/image")
+    @ResponseBody
+    public String getImage() {
+
+        try {
+            return image.getBase64Encoded();
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     @ResponseBody
     public String transform(
             @RequestParam("transform") String transform
             , @RequestParam(value="cropParams", required = false) String cropParams
     ) throws UnsupportedEncodingException {
-        System.out.println(transform);
+
         cropParams = cropParams == null ? null : URLDecoder.decode(cropParams, "UTF-8");
-        System.out.println(cropParams);
 
         System.out.println("Transform : " + transform);
 
         try {
             image.convert(transform, cropParams);
-            return "data:image/jpeg;base64," + image.getBase64Encoded();
+            return image.getBase64Encoded();
         } catch (IOException | IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            return "erorr";
+            return "error";
         }
 
     }
